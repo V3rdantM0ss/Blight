034fb3152f7b32517b212f9dcc7fb0198193c5bf
verdant_moss <VerdantMoss@gmx.de>
2026-03-06 09:50:25 +0100

    Added LibGDX and made a tiny window creation.

--- src/main/java/verdant_moss/blight/Blight.java
+++ src/main/java/verdant_moss/blight/Blight.java
@@ -1,17 +1,29 @@
 package verdant_moss.blight;
 
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import verdant_moss.hollow.Color;
 import verdant_moss.hollow.Hollow;
 import verdant_moss.hollow.ReleaseType;
 import verdant_moss.hollow.Version;
 
-public class Blight {
+public class Blight extends ApplicationAdapter {
 	public static final String BLIGHT_NAME = "Blight";
-	public static final Version BLIGHT_VERSION = new Version(0,0,0,0, ReleaseType.ALPHA);
+	public static final Version BLIGHT_VERSION = new Version(0, 0, 0, 1, ReleaseType.ALPHA);
 	public static final Color BLIGHT_COLOR = new Color(137, 77, 252);
 	public static final Hollow BLIGHT_HOLLOW = new Hollow(BLIGHT_NAME, BLIGHT_VERSION, BLIGHT_COLOR);
+	private String name;
+	private int width, height;
 	
-	public Blight() {
-		BLIGHT_HOLLOW.get_aurora().info("wawa :D");
+	public Blight(String name, int width, int height) {
+		this.name = name;
+		this.width = width;
+		this.height = height;
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle(name);
+		config.setWindowedMode(width, height);
+		config.useVsync(true);
+		new Lwjgl3Application(this, config);
 	}
 }
--- src/test/java/verdant_moss/blight/BlightTest.java
+++ src/test/java/verdant_moss/blight/BlightTest.java
@@ -2,6 +2,6 @@ package verdant_moss.blight;
 
 public class BlightTest {
 	public static void main(String[] args) {
-		new Blight();
+		new Blight("Test", 600, 600);
 	}
 }
